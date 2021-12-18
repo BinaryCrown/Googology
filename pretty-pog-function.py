a89ecc600aec050647f000a2b0bf17e44001c078
BinaryCrown <93054701+BinaryCrown@users.noreply.github.com>
2021-12-18 09:14:12 +0000

    More golfing!

--- pretty-pog-function.py
+++ pretty-pog-function.py
@@ -1,9 +1,4 @@
-def e(l):
-    o=2*l[0]+1
-    for m in range(1,len(l)):o*=2**l[m]
-    return o
-def m(l,i):
-    if i:return multi_encode([int(k) for k in [char for char in str(multi_encode(l,i-1))]],i-1)
-    else:return e(l)
-def PPF(k):return m([3,5,7],k)
-print(PPF(5))
+import math
+e=lambda l:(2*l[0]+1)*math.prod([2**i for i in l])
+m = lambda l,i:multi_encode([int(k) for k in [char for char in str(multi_encode(l,i-1))]],i-1) if i else e(l)
+x=m([3,5,7],5)
