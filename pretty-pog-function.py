a0c0f2d222f11c091518b66d6e41cd704a4412b6
BinaryCrown <93054701+BinaryCrown@users.noreply.github.com>
2021-12-18 09:16:21 +0000

    More golfing lol

--- pretty-pog-function.py
+++ pretty-pog-function.py
@@ -1,4 +1,4 @@
 import math
 e=lambda l:(2*l[0]+1)*math.prod([2**i for i in l])
-m=lambda l,i:m([int(k) for k in [c for c in str(m(l,i-1))]],i-1) if i else e(l)
+m=lambda l,i:m([int(k) for k in str(m(l,i-1))],i-1)if i else e(l)
 x=m([3,5,7],5)
