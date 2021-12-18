0dc279e0bfb80e565b3d598175ac96fe04189ef8
BinaryCrown <93054701+BinaryCrown@users.noreply.github.com>
2021-12-18 09:08:30 +0000

    Did some golfing

--- pretty-pog-function.py
+++ pretty-pog-function.py
@@ -1,16 +1,9 @@
-def encode(l):
-    o = 2*l[0]+1
-    for m in range(1,len(l)):
-        o *= 2**l[m]
+def e(l):
+    o=2*l[0]+1
+    for m in range(1,len(l)):o*=2**l[m]
     return o
-
-def multi_encode(l,i):
-    if i==1:
-        return encode(l)
-    else:
-        return multi_encode([int(k) for k in [char for char in str(multi_encode(l,i-1))]],i-1)
-
-def PPF(k):
-    return multi_encode([3,5,7],k+1)
-
+def m(l,i):
+    if i:return multi_encode([int(k) for k in [char for char in str(multi_encode(l,i-1))]],i-1)
+    else:return e(l)
+def PPF(k):return m([3,5,7],k)
 print(PPF(5))
