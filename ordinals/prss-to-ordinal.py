fa2cd4467587ce754a08d6e069f821cacdb39ab8
BinaryCrown <93054701+BinaryCrown@users.noreply.github.com>
2022-04-11 10:45:00 +0100

    Removed standardness checks as they don't work

--- ordinals/prss-to-ordinal.py
+++ ordinals/prss-to-ordinal.py
@@ -1,26 +1,6 @@
 # Converts primitive sequences to ordinals
 
-def lexicoless(l: list, r: list) -> bool:
-    if l == []: return r != []
-    if r == []: return False
-    else: return l[0] < r[0] and (l[0] == r[0] or lexicoless(l[1:], r[1:]))
-
-def isstd(l: list) -> bool:
-    if len(l) != 0:
-        if l[0] != 0:
-            return False
-        for k in range(len(l)):
-            if not type(k) is int: raise "Invalid."
-            if k < len(l)-1 and l[k+1]-l[k] > 1: return False
-            for j in range(k,len(l)):
-                if l[j] == l[k] and lexicoless(l[k:],l[j:]): return False
-        return True
-    return True
-
 def basicToOrdinal(l: list) -> str:
-    if not isstd(l):
-        raise "Not standard."
-    
     if len(l) == 0: return "0"
     
     if any([k == 0 for k in l[1:]]):
